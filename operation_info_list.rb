1e86d842fc508447c62b4edbdc0d48c7816c9b87
KomureYuta <komure@KomurenoMacBook-puro.local>
2018-05-16 14:11:15 +0900

    追加：路線名をひらがな表示

--- operation_info_list.rb
+++ operation_info_list.rb
@@ -2,5 +2,8 @@ class OperationInfoList
 	attr_reader :operation_info_list
 	def initialize(operation_info_list)
 		@operation_info_list = operation_info_list
-  end
+	end
+	def get_message_sorted_by_line_name
+		operation_info_list.sort_by {|info| info.line_name_kana}
+	end
 end
--- operation_info_repository.rb
+++ operation_info_repository.rb
@@ -5,6 +5,6 @@ class OperationInfoRepository
 
     def self.get_operation_info
         all_operation_info = CSV.read("test.csv")
-        OperationInfoList.new(all_operation_info.map{|info| OperationInfo.new(info[0], info[1], info[2])})
+        OperationInfoList.new(all_operation_info.map{|info| OperationInfo.new(info[0], info[1], info[2], info[3])})
     end
 end
